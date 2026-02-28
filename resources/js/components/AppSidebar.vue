a8ad5922621bd6566efacb896530dda2831a45e8
muhohoweb <muhohoweb@icloud.com>
2026-02-28 21:19:25 +0300

    Add packages

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -2,132 +2,146 @@
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { type NavItem } from '@/types';
-import { Link } from '@inertiajs/vue3';
 import {
-    LayoutGrid,
-    Settings,
-    FileText,
-    Calendar1Icon,
-    UsersRoundIcon,
-    HouseIcon,
-    ShieldAlertIcon,
-    StethoscopeIcon,
-    CircleUserIcon,
-    PencilIcon,
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem
+} from '@/components/ui/sidebar';
+import {type NavItem} from '@/types';
+import {Link} from '@inertiajs/vue3';
+import {
+  LayoutGrid,
+  Settings,
+  FileText,
+  Calendar1Icon,
+  UsersRoundIcon,
+  HouseIcon,
+  ShieldAlertIcon,
+  StethoscopeIcon,
+  CircleUserIcon,
+  PencilIcon,
 } from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
-import { computed } from 'vue';
-import { useAuth } from '@/utils/auth';
+import {computed} from 'vue';
+import {useAuth} from '@/utils/auth';
 
 const user = useAuth();
 
 const allNavItems = [
-    {
-        title: 'Dashboard',
-        href: '/dashboard',
-        icon: LayoutGrid,
-    },
-    {
-        title: 'Jobs',
-        href: '/health-jobs',
-        icon: StethoscopeIcon,
-        requiredPermissions: ['view-job-postings'],
-    },
-    {
-        title: 'Profiles',
-        href: '/medics/profiles',
-        icon: CircleUserIcon,
-        requiredPermissions: ['view-job-postings'],
-    },
-    {
-        title: 'Events',
-        href: '/events',
-        disabled: true,
-        icon: Calendar1Icon,
-        requiredPermissions: ['view-job-postings'],
-    },
+  {
+    title: 'Dashboard',
+    href: '/dashboard',
+    icon: LayoutGrid,
+  },
+  {
+    title: 'Jobs',
+    href: '/health-jobs',
+    icon: StethoscopeIcon,
+    requiredPermissions: ['view-job-postings'],
+  },
+  {
+    title: 'Profiles',
+    href: '/medics/profiles',
+    icon: CircleUserIcon,
+    requiredPermissions: ['view-job-postings'],
+  },
+  {
+    title: 'Events',
+    href: '/events',
+    disabled: true,
+    icon: Calendar1Icon,
+    requiredPermissions: ['view-job-postings'],
+  },
 
-    {
-        title: 'Feedback',
-        href: '/feedback',
-        disabled: true,
-        icon: PencilIcon,
-        requiredPermissions: ['view-job-postings'],
-    },
-    {
-        title: 'Users',
-        href: '/iam',
-        icon: UsersRoundIcon,
-        requiredRoles: ['super-admin'],
-    },
-    {
-        title: 'Roles',
-        href: '/iam/roles',
-        icon: ShieldAlertIcon,
-        requiredRoles: ['super-admin'],
-    },
-    {
-        title: 'Manage Facility',
-        href: '/facilities',
-        icon: HouseIcon,
-        requiredRoles: ['super-admin'],
-    },
-    {
-        title: 'Reports',
-        href: '/reports',
-        icon: FileText,
-        requiredPermissions: ['view reports', 'generate reports'],
-        requireAnyPermission: false,
-    },
-    {
-        title: 'Settings',
-        href: '/settings',
-        icon: Settings,
-        requiredRoles: ['admin'],
-        requiredPermissions: ['manage settings'],
-    },
+  {
+    title: 'Feedback',
+    href: '/feedback',
+    disabled: true,
+    icon: PencilIcon,
+    requiredPermissions: ['view-job-postings'],
+  },
+  {
+    title: 'Users',
+    href: '/iam',
+    icon: UsersRoundIcon,
+    requiredRoles: ['super-admin'],
+  },
+  {
+    title: 'Packages',
+    href: '/admin/packages',
+    icon: UsersRoundIcon,
+    requiredRoles: ['super-admin'],
+  },
+  {
+    title: 'Roles',
+    href: '/iam/roles',
+    icon: ShieldAlertIcon,
+    requiredRoles: ['super-admin'],
+  },
+  {
+    title: 'Manage Facility',
+    href: '/facilities',
+    icon: HouseIcon,
+    requiredRoles: ['super-admin'],
+  },
+  {
+    title: 'Reports',
+    href: '/reports',
+    icon: FileText,
+    requiredPermissions: ['view reports', 'generate reports'],
+    requireAnyPermission: false,
+  },
+  {
+    title: 'Settings',
+    href: '/settings',
+    icon: Settings,
+    requiredRoles: ['admin'],
+    requiredPermissions: ['manage settings'],
+  },
 ];
 
 const mainNavItems = computed((): NavItem[] => {
-    return allNavItems.filter((item) => {
-        const requirements = {
-            requiredRoles: item.requiredRoles || [],
-            requiredPermissions: item.requiredPermissions || [],
-            requireAnyRole: item.requireAnyRole || false,
-            requireAnyPermission: item.requireAnyPermission || false,
-        };
+  return allNavItems.filter((item) => {
+    const requirements = {
+      requiredRoles: item.requiredRoles || [],
+      requiredPermissions: item.requiredPermissions || [],
+      requireAnyRole: item.requireAnyRole || false,
+      requireAnyPermission: item.requireAnyPermission || false,
+    };
 
-        return user.canAccess(requirements);
-    });
+    return user.canAccess(requirements);
+  });
 });
 
 const footerNavItems: NavItem[] = [];
 </script>
 
 <template>
-    <Sidebar collapsible="icon" variant="inset">
-        <SidebarHeader>
-            <SidebarMenu>
-                <SidebarMenuItem>
-                    <SidebarMenuButton size="lg" as-child>
-                        <Link :href="route('dashboard')">
-                            <AppLogo />
-                        </Link>
-                    </SidebarMenuButton>
-                </SidebarMenuItem>
-            </SidebarMenu>
-        </SidebarHeader>
+  <Sidebar collapsible="icon" variant="inset">
+    <SidebarHeader>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <SidebarMenuButton size="lg" as-child>
+            <Link :href="route('dashboard')">
+              <AppLogo/>
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarHeader>
 
-        <SidebarContent>
-            <NavMain :items="mainNavItems" />
-        </SidebarContent>
+    <SidebarContent>
+      <NavMain :items="mainNavItems"/>
+    </SidebarContent>
 
-        <SidebarFooter>
-            <NavFooter :items="footerNavItems" />
-            <NavUser />
-        </SidebarFooter>
-    </Sidebar>
-    <slot />
+    <SidebarFooter>
+      <NavFooter :items="footerNavItems"/>
+      <NavUser/>
+    </SidebarFooter>
+  </Sidebar>
+  <slot/>
 </template>
